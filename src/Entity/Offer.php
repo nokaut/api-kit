bb31222d62a50d96c23653091c963fba42845588
Jarosław Juszkiewicz <jaroslaw.juszkiewicz@nokaut.pl>
2014-09-22 11:14:12 +0200

    join id in offer

--- src/Entity/Offer.php
+++ src/Entity/Offer.php
@@ -114,6 +114,10 @@ class Offer extends EntityAbstract
      * @var Shop
      */
     protected $shop;
+    /**
+     * @var string
+     */
+    protected $join_id;
 
     /**
      * @param boolean $availability
@@ -514,5 +518,22 @@ class Offer extends EntityAbstract
     {
         return $this->warranty;
     }
+
+    /**
+     * @param string $join_id
+     */
+    public function setJoinId($join_id)
+    {
+        $this->join_id = $join_id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getJoinId()
+    {
+        return $this->join_id;
+    }
+
 
 }
