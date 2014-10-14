4021cfdb022677eef3b417fd5fa7592a95b202b6
Jarosław Juszkiewicz <jaroslaw.juszkiewicz@nokaut.pl>
2014-10-14 14:21:47 +0200

    set parent category i callbacks

---
+++ tests/Ext/Data/Converter/Filters/Callback/Categories/SetParentCategoryTest.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jjuszkiewicz
+ * Date: 14.10.2014
+ * Time: 14:05
+ */
+
+namespace Nokaut\ApiKit\Ext\Data\Converter\Filters\Callback\Categories;
+
+
+
+use Nokaut\ApiKit\Collection\Products;
+use Nokaut\ApiKit\Entity\Category;
+use Nokaut\ApiKit\Ext\Data\Collection\Filters\Categories;
+
+class SetParentCategoryTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSetParentCategory()
+    {
+        $categories = new Categories(array());
+        $cut = new SetParentCategory($this->prepareCurrentCategory());
+
+
+        $cut($categories,  new Products(array()));
+
+        $parentCategory = $categories->getParentCategory();
+        $parentCategoryPath = $this->prepareParentCategoryPath();
+        $this->assertNotEmpty($parentCategory);
+        $this->assertEquals($parentCategory->getName(), $parentCategoryPath->getTitle());
+        $this->assertEquals($parentCategory->getUrl(), $parentCategoryPath->getUrl());
+        $this->assertEquals($parentCategory->getIsFilter(), false);
+        $this->assertEquals($parentCategory->getIsNofollow(), false);
+    }
+
+    protected function prepareCurrentCategory()
+    {
+
+        $parentCategoryPath = $this->prepareParentCategoryPath();
+
+        $category = new Category();
+        $category->setId(2);
+        $category->setParentId($parentCategoryPath->getId());
+        $category->setTitle('Current Category');
+
+        $pathList = array();
+
+        $path = new Category\Path();
+        $path->setTitle("Category 30");
+        $path->setId(30);
+        $path->setUrl('/category30');
+        $pathList[] = $path;
+
+        $pathList[] = $parentCategoryPath;
+
+        $path = new Category\Path();
+        $path->setTitle("Current Category");
+        $path->setId(2);
+        $path->setUrl('/category2');
+        $pathList[] = $path;
+
+        $category->setPath($pathList);
+
+        return $category;
+    }
+
+    /**
+     * @return Category\Path
+     */
+    protected function prepareParentCategoryPath()
+    {
+        $path = new Category\Path();
+        $path->setTitle("Category 1");
+        $path->setId(1);
+        $path->setUrl('/category1');
+        return $path;
+    }
+} 
